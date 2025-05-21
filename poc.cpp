90689ecfc69c17d79efd7f6346cb1801b961760a
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-21 17:40:46 +0100

    dumping accessor info

--- poc.cpp
+++ poc.cpp
@@ -22,6 +22,7 @@ int main() {
   using namespace jason::ast::nodes;
   auto json = jason::parse(json_src);
   auto & root = cast<dict>(json);
+
   for (auto & b : cast<array>(root["buffers"])) {
     auto & bd = cast<dict>(b);
     auto len = cast<number>(bd["byteLength"]).integer();
@@ -35,6 +36,19 @@ int main() {
       ? cast<number>(bd["byteOffset"]).integer()
       : 0;
     putln("buffer view len: ", len, ", ofs: ", ofs, ", id: ", buf);
+  }
+  for (auto & a : cast<array>(root["accessors"])) {
+    auto & ad = cast<dict>(a);
+    auto bv = ad.has_key("bufferView")
+      ? cast<number>(ad["bufferView"]).integer()
+      : 0;
+    auto ofs = ad.has_key("byteOffset")
+      ? cast<number>(ad["byteOffset"]).integer()
+      : 0;
+    auto ctype = cast<number>(ad["componentType"]).integer();
+    auto count = cast<number>(ad["count"]).integer();
+    auto type = cast<string>(ad["type"]).str();
+    putln("accessor view: ", bv, ", ofs: ", ofs, ", ctype: ", ctype, ", count: ", count, ", type: ", type);
   }
 
   auto sid = cast<number>(root["scene"]).integer();
