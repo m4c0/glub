d4aea22d9b7281528f29beb92ab1ea085d3f28a3
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-26 22:19:22 +0100

    vulkan: trying to use buffers

--- poc-vulkan.cpp
+++ poc-vulkan.cpp
@@ -4,6 +4,7 @@
 
 import glub;
 import hai;
+import silog;
 import vapp;
 import voo;
 
@@ -30,7 +31,8 @@ struct i : public vapp {
       hai::array<vee::buffer> bufs { bvs.size() };
       for (auto i = 0; i < bvs.size(); i++) {
         auto [ofs, len] = bvs[i];
-        bufs[i] = vee::create_buffer(len, vee::buffer_usage::vertex_buffer);
+        bufs[i] = vee::create_buffer(len, vee::buffer_usage::vertex_buffer, vee::buffer_usage::index_buffer);
+        vee::bind_buffer_memory(*bufs[i], *mem, ofs);
       }
 
       auto pl = vee::create_pipeline_layout();
@@ -43,16 +45,37 @@ struct i : public vapp {
           voo::shader("poc-vulkan.frag.spv").pipeline_frag_stage(),
         },
         .bindings {
-          vee::vertex_input_bind(1), // TODO: stride
+          vee::vertex_input_bind(3 * sizeof(float)),
         },
         .attributes {
-          vee::vertex_attribute_float(0, 0), // TODO: offset
+          vee::vertex_attribute_vec3(0, 0),
         },
       });
+
+      auto scene = meta.main_scene();
 
       extent_loop(dq.queue(), sw, [&] {
         sw.queue_one_time_submit(dq.queue(), [&](auto pcb) {
           auto scb = sw.cmd_render_pass({ *pcb });
+
+          vee::cmd_bind_gr_pipeline(*scb, *ppl);
+          glub::visit_meshes(scene, [&](auto m) {
+            auto mesh = meta.mesh(m);
+
+            for (auto & prim : mesh.prims) {
+              if (!prim.position.count) throw 0; // TODO errors
+              if (!prim.indices.count) throw 0; // TODO errors
+
+              auto buf = *bufs[prim.position.buffer_view];
+              auto ofs = prim.position.offset;
+              vee::cmd_bind_vertex_buffers(*scb, 0, buf, ofs);
+
+              buf = *bufs[prim.indices.buffer_view];
+              ofs = prim.indices.offset;
+              vee::cmd_bind_index_buffer_u16(*scb, buf, ofs);
+              vee::cmd_draw(*scb, prim.indices.count);
+            }
+          });
         });
       });
     });
