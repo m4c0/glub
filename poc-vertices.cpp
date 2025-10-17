7182c8e6994fc480bd70c805939a038fcc3e4bed
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-17 18:10:16 +0100

    poc-vtx: proper recursion

--- poc-vertices.cpp
+++ poc-vertices.cpp
@@ -4,6 +4,22 @@ import glub;
 import jojo;
 import print;
 
+static void recurse(const glub::t & t, int n) {
+  if (n < 0 || n >= t.nodes.size()) die("node index out-of-bounds");
+  auto & node = t.nodes[n];
+
+  put(n, " ");
+  if (node.matrix.size()) put("m");
+  if (node.rotation.size()) put("r");
+  if (node.scale.size()) put("s");
+  if (node.translation.size()) put("t");
+  putln();
+
+  for (auto child : node.children) {
+    recurse(t, child);
+  }
+}
+
 int main() {
   auto src = jojo::read("models/BoxAnimated.glb");
   const auto t = glub::parse(src.begin(), src.size());
@@ -11,24 +27,7 @@ int main() {
   if (t.scene < 0) die("missing main scene");
   if (t.scene >= t.scenes.size()) die("main scene index out-of-bounds");
 
-  for (auto node_idx : t.scenes[t.scene].nodes) {
-    if (node_idx < 0 || node_idx >= t.nodes.size()) die("node index out-of-bounds");
-    auto & node = t.nodes[node_idx];
-
-    putln("node: ", node_idx, " ", node.name);
-
-    for (auto child : node.children) {
-      if (child < 0 || child >= t.nodes.size()) die("node index out-of-bounds");
-      auto & node = t.nodes[child];
-
-      putln(" node: ", child, " ", node.name);
-
-      for (auto gchild : node.children) {
-        if (gchild < 0 || gchild >= t.nodes.size()) die("node index out-of-bounds");
-        auto & node = t.nodes[gchild];
-
-        putln("  node: ", gchild, " ", node.name);
-      }
-    }
+  for (auto n : t.scenes[t.scene].nodes) {
+    recurse(t, n);
   }
 }
