c7b1dbee3d1e133ef746446a2a78d0b0a3083bc5
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-17 15:56:06 +0100

    poc2: meshes and primitives (without morph target)

--- poc2.cpp
+++ poc2.cpp
@@ -25,6 +25,22 @@ struct chunk {
   uint32_t type;
 };
 
+struct attribute {
+  jute::heap key {};
+  int accessor = -1;
+};
+struct primitive {
+  hai::array<attribute> attributes;
+  int indices = -1;
+  int material = -1;
+  int mode = 4;
+  hai::array<jute::heap> targets {};
+};
+struct mesh {
+  jute::heap name {};
+  hai::array<primitive> primitives {};
+  hai::array<float> weights {};
+};
 struct node {
   jute::heap name {};
   int camera = -1;
@@ -43,6 +59,7 @@ struct scene {
 };
 struct t {
   int scene = -1;
+  hai::array<::mesh> meshes {};
   hai::array<::node> nodes {};
   hai::array<::scene> scenes {};
 };
@@ -96,26 +113,53 @@ int main() try {
   const auto parse_int = [&](auto & n, jute::view key, auto & v) {
     if (n.has_key(key)) v = cast<number>(n[key]).integer();
   };
+  const auto parse_string = [&](auto & n, jute::view key, auto & v) {
+    if (n.has_key(key)) v = cast<string>(n[key]).str();
+  };
 
+  if (root.has_key("meshes")) {
+    auto & meshes = cast<array>(root["meshes"]);
+    t.meshes.set_capacity(meshes.size());
+    for (auto i = 0; i < meshes.size(); i++) {
+      auto & n = cast<dict>(meshes[i]);
+      parse_floats(n, "weights", t.meshes[i].weights);
+      parse_string(n, "name",    t.meshes[i].name);
+
+      auto & prims = cast<array>(n["primitives"]);
+      t.meshes[i].primitives.set_capacity(prims.size());
+      for (auto j = 0; j < t.meshes[i].primitives.size(); j++) {
+        auto & prim = cast<dict>(prims[j]);
+
+        auto & attrs = cast<dict>(prim["attributes"]);
+        t.meshes[i].primitives[j].attributes.set_capacity(attrs.size());
+        auto ptr = t.meshes[i].primitives[j].attributes.begin();
+        for (auto &[k, v] : attrs) *ptr++ = { k, cast<number>(v).integer() };
+
+        parse_int(prim, "indices",  t.meshes[i].primitives[j].indices);
+        parse_int(prim, "material", t.meshes[i].primitives[j].material);
+        parse_int(prim, "mode",     t.meshes[i].primitives[j].mode);
+      }
+    }
+  }
   if (root.has_key("nodes")) {
     auto & nodes = cast<array>(root["nodes"]);
     t.nodes.set_capacity(nodes.size());
     for (auto i = 0; i < nodes.size(); i++) {
       auto & n = cast<dict>(nodes[i]);
-      parse_int(n, "camera", t.nodes[i].camera);
-      parse_ints(n, "children", t.nodes[i].children);
-      parse_int(n, "skin", t.nodes[i].skin);
-      parse_floats(n, "matrix", t.nodes[i].matrix);
-      parse_int(n, "mesh", t.nodes[i].mesh);
-      parse_floats(n, "rotation", t.nodes[i].rotation);
-      parse_floats(n, "scale", t.nodes[i].scale);
+      parse_int   (n, "camera",      t.nodes[i].camera);
+      parse_ints  (n, "children",    t.nodes[i].children);
+      parse_int   (n, "skin",        t.nodes[i].skin);
+      parse_floats(n, "matrix",      t.nodes[i].matrix);
+      parse_int   (n, "mesh",        t.nodes[i].mesh);
+      parse_floats(n, "rotation",    t.nodes[i].rotation);
+      parse_floats(n, "scale",       t.nodes[i].scale);
       parse_floats(n, "translation", t.nodes[i].translation);
-      parse_floats(n, "weights", t.nodes[i].weights);
-      if (n.has_key("name")) t.nodes[i].name = cast<string>(n["name"]).str();
+      parse_floats(n, "weights",     t.nodes[i].weights);
+      parse_string(n, "name",        t.nodes[i].name);
 
-      if (!t.nodes[i].matrix.size()) t.nodes[i].matrix = hai::array<float>::make(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1);
-      if (!t.nodes[i].rotation.size()) t.nodes[i].rotation = hai::array<float>::make(0, 0, 0, 1);
-      if (!t.nodes[i].scale.size()) t.nodes[i].scale = hai::array<float>::make(1, 1, 1);
+      if (!t.nodes[i].matrix.size())      t.nodes[i].matrix      = hai::array<float>::make(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1);
+      if (!t.nodes[i].rotation.size())    t.nodes[i].rotation    = hai::array<float>::make(0, 0, 0, 1);
+      if (!t.nodes[i].scale.size())       t.nodes[i].scale       = hai::array<float>::make(1, 1, 1);
       if (!t.nodes[i].translation.size()) t.nodes[i].translation = hai::array<float>::make(0, 0, 0);
     }
   }
@@ -125,7 +169,7 @@ int main() try {
     t.scenes.set_capacity(scenes.size());
     for (auto i = 0; i < scenes.size(); i++) {
       auto & s = cast<dict>(scenes[i]);
-      if (s.has_key("name")) t.scenes[i].name = cast<string>(s["name"]).str();
+      parse_string(s, "name", t.scenes[i].name);
       parse_ints(s, "nodes", t.scenes[i].nodes);
     }
   }
@@ -135,6 +179,19 @@ int main() try {
     putln();
   };
 
+  putln("meshes:");
+  for (auto & s : t.meshes) {
+    putln("- ", s.name);
+    put("  weights: "); list(s.weights);
+    putln("  primitives:");
+    for (auto & r : s.primitives) {
+      putln("  - ind:", r.indices, " mat:", r.material, " mode:", r.mode);
+      putln("    attributes:");
+      for (auto & q : r.attributes) putln("    - ", q.key, " = ", q.accessor);
+      putln("    targets:");
+      for (auto & q : r.targets) putln("    - ", q);
+    }
+  }
   putln("nodes:");
   for (auto & n : t.nodes) {
     put("- ", n.name, " ");
