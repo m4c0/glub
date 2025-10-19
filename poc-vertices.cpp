0e932e934afc43304e2b180f42c5cbb9cc38da4f
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-19 02:04:53 +0100

    poc-vtx: reading position and indices

--- poc-vertices.cpp
+++ poc-vertices.cpp
@@ -8,11 +8,39 @@ import print;
 
 static auto & accessor(const glub::t & t, int n) {
   if (n < 0 || n >= t.accessors.size()) die("accessor index out-of-bounds");
-  return t.accessors[n];
+  auto & a = t.accessors[n];
+  if (a.normalised) die("normalised accessors are not supported");
+  return a;
 }
+static auto & accessor_vec3(const glub::t & t, int n) {
+  auto & acc = accessor(t, n);
+  if (acc.component_type != glub::accessor_comp_type::flt) die("unsupported accessor component type");
+  if (acc.type != "VEC3") die("unsupported accessor type");
+  return acc;
+}
+
 static auto & buffer_view(const glub::t & t, int n) {
   if (n < 0 || n >= t.buffer_views.size()) die("buffer views index out-of-bounds");
-  return t.buffer_views[n];
+  auto & bv = t.buffer_views[n];
+  if (bv.buffer != 0) die("invalid buffer id");
+  return bv;
+}
+static auto & buffer_view_array_buffer(const glub::t & t, int n) {
+  auto & bv = buffer_view(t, n);
+  if (bv.target != glub::buffer_view_type::array_buffer) die("invalid buffer view type");
+  return bv;
+}
+static auto & buffer_view_element_array_buffer(const glub::t & t, int n) {
+  auto & bv = buffer_view(t, n);
+  if (bv.target != glub::buffer_view_type::element_array_buffer) die("invalid buffer view type");
+  return bv;
+}
+
+template<typename T>
+static auto cast(auto & acc, auto & bv, auto & t) {
+  auto ptr = reinterpret_cast<const T *>(t.data.begin() + acc.byte_offset + bv.byte_offset);
+  if ((void *)(ptr + acc.count) > t.data.end()) die("buffer overrun");
+  return ptr;
 }
 
 int main() {
@@ -22,24 +50,30 @@ int main() {
   for (auto & m : t.meshes) {
     for (auto & p : m.primitives) {
       if (p.mode != glub::primitive_mode::triangles) die("unsupported primitive mode");
+      if (p.indices < 0) die("missing indices in mesh");
 
       for (auto &[key, a] : p.attributes) {
         if (key != "POSITION") continue;
 
-        auto & acc = accessor(t, a);
-        if (acc.component_type != glub::accessor_comp_type::flt) die("unsupported accessor component type");
-        if (acc.normalised) die("normalised accessors are not supported");
-        if (acc.type != "VEC3") die("unsupported accessor type");
+        auto & acc = accessor_vec3(t, a);
+        auto & bv = buffer_view_array_buffer(t, acc.buffer_view);
+        auto ptr = cast<dotz::vec3>(acc, bv, t);
 
-        auto & bv = buffer_view(t, acc.buffer_view);
-        if (bv.buffer != 0) die("invalid buffer id");
-        if (bv.target != glub::buffer_view_type::array_buffer) die("invalid buffer view type");
-
-        auto ptr = reinterpret_cast<const dotz::vec3 *>(t.data.begin() + acc.byte_offset + bv.byte_offset);
-        if ((void *)(ptr + acc.count) > t.data.end()) die("buffer overrun");
-
-        putan(acc.count, acc.byte_offset, bv.byte_offset);
+        //for (auto i = 0; i < acc.count; i++) {
+        //  putan(ptr[i].x, ptr[i].y, ptr[i].z);
+        //}
       }
+
+      auto & acc = accessor(t, p.indices);
+      if (acc.type != "SCALAR") die("unsupported accessor type");
+      if (acc.component_type == glub::accessor_comp_type::unsigned_shrt) {
+        auto & bv = buffer_view_element_array_buffer(t, acc.buffer_view);
+        auto ptr = cast<unsigned short>(acc, bv, t);
+        //for (auto i = 0; i < acc.count; i++) putln(ptr[i]);
+      } else if (acc.component_type == glub::accessor_comp_type::unsigned_int) {
+        auto & bv = buffer_view_element_array_buffer(t, acc.buffer_view);
+        auto ptr = cast<unsigned int>(acc, bv, t);
+      } else die("unsupported accessor component type");
     }
   }
 }
