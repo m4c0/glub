aed24a3f0d02891c0d74230e2f1f958b25c8f2e3
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-22 21:36:39 +0100

    preparing loop for tesselation

--- poc-vertices.cpp
+++ poc-vertices.cpp
@@ -7,7 +7,8 @@ import jute;
 import print;
 
 template<typename T>
-static auto cast(auto & acc, auto & bv, auto & t) {
+static auto cast(auto & acc, auto & t) {
+  auto & bv = t.buffer_views[acc.buffer_view];
   auto ptr = reinterpret_cast<const T *>(t.data.begin() + acc.byte_offset + bv.byte_offset);
   if ((void *)(ptr + acc.count) > t.data.end()) die("buffer overrun");
   return ptr;
@@ -19,7 +20,32 @@ int main() {
 
   for (auto & m : t.meshes) {
     for (auto & p : m.primitives) {
-
+      put(t.accessors[p.accessors.position].count);
+      put(' ');
+      putln(t.accessors[p.indices].count);
+    }
+  }
+  for (auto & m : t.meshes) {
+    for (auto & p : m.primitives) {
+      auto & a = p.accessors;
+      if (a.position >= 0) {
+        auto & acc = t.accessors[a.position];
+        auto p = cast<dotz::vec3>(acc, t);
+        for (auto i = 0; i < acc.count; i++) {
+        }
+      }
+      if (a.normal >= 0) {
+        auto & acc = t.accessors[a.normal];
+        auto p = cast<dotz::vec3>(acc, t);
+        for (auto i = 0; i < acc.count; i++) {
+        }
+      }
+      if (a.texcoord_0 >= 0) {
+        auto & acc = t.accessors[a.texcoord_0];
+        auto p = cast<dotz::vec2>(acc, t);
+        for (auto i = 0; i < acc.count; i++) {
+        }
+      }
     }
   }
 }
